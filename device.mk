6e82b4e56e9839a0153ebfd9fe49f456cda14040
Rocky7842 <eric.rocky7842@gmail.com>
2024-07-19 20:04:25 +0800

    psyche: Support UDFPS sensors sub-HAL
    Change-Id: I320c060a6f4c71d353db9b3c125649d37f207b6d

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
